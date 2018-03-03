d6c54e3c89c8c597972703e85bd9596d016c37f3
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-02 23:36:53 -0500

    ngrx cleared

--- CounterNgrx/src/app/app.module.ts
+++ CounterNgrx/src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-// import {StoreModule} from '@ngrx/store';
+// import { StoreModule } from '@ngrx/store';
 // import { counterReducer } from './store/counter';
-
-// export const reducers: ActionReducerMap<any> = {
-// count: counterReducer.counterReducer
-// };
 
 @NgModule({
   declarations: [
@@ -20,8 +13,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
     AppRoutingModule,
     // StoreModule.forRoot({ count: counterReducer })
   ],
